a8c1178d11d35eb4f887b0e90a93e8b59da45505
unknown <singh.raviranjan6@gmail.com>
2019-01-29 16:59:32 +0530

    v1.0

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlwebpackPlugin=require('html-webpack-plugin');
+module.exports= {
+    entry: ['babel-polyfill','./src/js/index'],
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:'js/bundle.js'
+    },
+    devServer:{
+        contentBase : './dist'
+    },
+    plugins:[
+        new HtmlwebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
